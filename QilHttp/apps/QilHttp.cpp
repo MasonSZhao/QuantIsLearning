8a55b8ef26a1ee42863a18fc772141f9c8ea5c2e
MasonSZhao <MasonSZhao@qq.com>
2025-10-05 16:52:26 +0800

    添加http应用程序入口。 add the http application entry point.

---
+++ QilHttp/apps/QilHttp.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2025-present Mason Zhao MasonSZhao@qq.com
+
+// QilHttp.cpp : Defines the entry point for the application.
+
+#include "QilHttp.h"
+
+int main()
+{
+    std::cout << "Hello QilHttp CMake." << std::endl;
+    return 0;
+}
---
+++ QilHttp/apps/QilHttp.h
@@ -0,0 +1,7 @@
+﻿// Copyright (c) 2025-present Mason Zhao MasonSZhao@qq.com
+
+// QilHttp.h : Include file for standard system include files, or project specific include files.
+
+#pragma once
+
+#include <iostream>
